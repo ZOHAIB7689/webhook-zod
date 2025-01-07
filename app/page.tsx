e49441a4a44a2ac8d292a8801b037ddc64e2b67d
ZOHAIB7689 <youremail@example.com>
2025-01-07 10:18:53 +0500

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import Form from "./components/Form";
 
 function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello!</h1>
+    <main className="flex bg-gradient-to-br from-teal-200 via-rose-200 to-c2an-100 min-h-screen flex-col items-center justify-between p-24">
+      <Form />
     </main>
   );
 }
